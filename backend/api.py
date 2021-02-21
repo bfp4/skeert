ee0ac08530a9301a82f06a661ac9b534e5107285
bfp4 <66973969+bfp4@users.noreply.github.com>
2021-02-21 01:22:28 -0500

    update

--- backend/api.py
+++ backend/api.py
@@ -1,10 +1,23 @@
+import time
 from flask import Flask, render_template
+from python.get_freq import get_freq
+from python.read_csv import read_freq
+from python.spleeter import run_spleeter
+
+def main(music_file): 
+    print("running.")
+    run_spleeter(music_file)
+    print("spleeter done.")
+    get_freq("spleeter-output")
+    read_freq()
+    print("done")
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/time")
 def index():
-    return f"Hello World"
+    main("Purim-drip.mp3")
+    return {"time": time.time()}
 
 if __name__ == "__main__":
     app.run(debug=True)
--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import React, {useState, useEffect} from 'react'
+
 function App() {
+  const [file, setFile] = useState("")
+
+  const fileInserted = (event) => setFile(event.target.value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+  
   return (
     <div>
-      <form action="post">
-        <input type="file" />
+      <form onSubmit={handleSubmit}>
+        <input type="file" value={file} onChange={fileInserted}/>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
