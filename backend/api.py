5dd88bd16e23a3501fb5020ee46fad2ea074244b
bfp4 <66973969+bfp4@users.noreply.github.com>
2021-02-23 15:59:39 -0500

    files can sent

--- backend/api.py
+++ backend/api.py
@@ -5,7 +5,7 @@ from python.get_freq import get_freq
 from python.read_csv import read_freq
 from python.spleeter import run_spleeter
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
+UPLOAD_FOLDER = "uploads"
 ALLOWED_EXTENSIONS = {'wav', 'mp3'}
 
 app = Flask(__name__)
@@ -28,19 +28,15 @@ def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
-            return {'file': request.files}
+            return redirect(request.url)
         file = request.files['file']
         if file.filename == '':
             flash('No selected file')
-            return {'file': 'no selected file'}
+            return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-
-    else:
-        return {"file": "this a get"}
+            return redirect("http://localhost:3000/")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,17 @@
 import React, {useState} from 'react'
 
 function App() {
-  const [file, setFile] = useState(null);
-  const fileInserted = (event) => setFile(event.target.value);
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (file) {
-      const data = new FormData();
-      data.append("file", file);
-      await fetch("/run", {
-        method: "POST",
-        body: data,
-        headers: {
-          "Content-type": "multipart/form-data"
-        }
-      }).then(res => res.json())
-      .then(data => console.log(data.file))
-      .catch(error => console.error(error))
-    }
-  };
+  const [file, setFile] = useState(false)
+
+  const insertFile = () => setFile(true)
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form method="post" action="/run" encType="multipart/form-data">
         <input
           type="file"
           name="file"
-          onChange={fileInserted}
+          onChange={insertFile}
         />
         <button type="submit" disabled={!file}>Submit</button>
       </form>
